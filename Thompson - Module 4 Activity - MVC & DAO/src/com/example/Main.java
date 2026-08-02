8d88eefeae11ad9d90ccc42ead7b6036076beb19
Zachery T. <165421910+BindonWrathbite@users.noreply.github.com>
2026-08-02 14:51:39 -0400

    Added Module 4 Files

---
+++ Thompson - Module 4 Activity - MVC & DAO/src/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+import com.example.controller.AnimalController;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Create an instance of AnimalController and start the application
+        AnimalController animalController = new AnimalController();
+        animalController.start();
+    }
+    
+}
